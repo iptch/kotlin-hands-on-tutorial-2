63f2fb1f4d2f94bfe487e809636dea1833a0e2a2
Pascal <pascal.knecht@hotmail.ch>
2024-02-07 15:01:27 +0100

    upgrade project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.3"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.example.kotlin"
@@ -23,7 +23,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("com.github.javafaker:javafaker:1.0.2")
+	implementation("com.github.javafaker:javafaker:1.0.2") {
+		exclude(module = "snakeyaml")
+	}
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
