658cff32238868f923d15ae09dd1ee8fb57b3f07
Pascal <pascal.knecht@hotmail.ch>
2024-02-07 16:51:31 +0100

    upgrade project
    (cherry picked from commit 63f2fb1f4d2f94bfe487e809636dea1833a0e2a2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.32"
-	kotlin("plugin.spring") version "1.4.32"
+	id("org.springframework.boot") version "3.2.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
 }
 
 group = "com.example.kotlin"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
+
 
 repositories {
 	mavenCentral()
-	jcenter()
-	maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -25,7 +25,9 @@ dependencies {
 	implementation("io.r2dbc:r2dbc-h2")
 
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("com.github.javafaker:javafaker:1.0.2")
+	implementation("com.github.javafaker:javafaker:1.0.2") {
+		exclude(module = "snakeyaml")
+	}
 
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -37,7 +39,7 @@ dependencies {
 
 	runtimeOnly("com.h2database:h2")
 
-	implementation("org.jetbrains:markdown:0.2.2")
+	implementation("org.jetbrains:markdown:0.6.1")
 }
 
 tasks.withType<Test> {
@@ -47,6 +49,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
